b28395f8371bb2fbe4530c03fc79e019f8dd7741
Dnyanesh Kshatriya <141269640+DnyaneshKshatriya07@users.noreply.github.com>
2026-02-12 17:26:17 +0000

    clean whole unnecessary code from main code.

--- DemoMVCApp/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace DemoMVCApp.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
--- DemoMVCApp/Models/Student.cs
+++
@@ -1,9 +0,0 @@
-namespace DemoMVCApp.Models
-{
-    public class Student
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Course { get; set; }
-    }
-}
