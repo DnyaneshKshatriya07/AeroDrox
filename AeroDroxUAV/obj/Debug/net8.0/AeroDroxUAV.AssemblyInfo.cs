88a790dfc8e13c3b8b4c1902be1416486c722a1e
Dnyanesh Kshatriya <141269640+DnyaneshKshatriya07@users.noreply.github.com>
2026-02-12 17:32:02 +0000

    clean whole unnecessary code from main code.

--- AeroDroxUAV/obj/Debug/net8.0/AeroDroxUAV.AssemblyInfo.cs
+++ AeroDroxUAV/obj/Debug/net8.0/AeroDroxUAV.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AeroDroxUAV")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a26185ec282b9dcc60078bf436c9a798e028150a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b28395f8371bb2fbe4530c03fc79e019f8dd7741")]
 [assembly: System.Reflection.AssemblyProductAttribute("AeroDroxUAV")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AeroDroxUAV")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
